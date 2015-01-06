5b7c306ca22ad1657281d39d6331946c253185bd
dfconant <dfconant@gmail.com>
2015-01-06 08:59:23 -0800

    Finish core ERP creation, still need to improve visualization

--- ERP.py
+++ ERP.py
@@ -1,4 +1,4 @@
-#createERP.py
+#ERP.py
 
 
 import os
@@ -8,18 +8,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import scipy as sp
 import scipy.io
+import math
 
-def evnt2ERP(evnt):
-    
-    print('a')
-
-
-def mat2ERP(Ymat):
-    mat = sp.io.loadmat(Ymat);
-    D = mat['neuralY']
-    return D
-    
-    
 def plotERP(D,onset,fs):
 #Given a electrodesXtimeXtrials matrix, generates ERPs with 1 sec on 
 #either side of the (user defined) onset
@@ -27,31 +17,43 @@ def plotERP(D,onset,fs):
 #David Conant 12/17/14 
 
     ERP = np.nanmean(D,axis=2);
-    ERPerr = np.nanstd(D,axis=2)/D.shape[2];
+    ERPerr = np.nanstd(D,axis=2)/D.shape[2]
     
     minall = np.nanmin(D)
     maxall = np.nanmax(D)
     
-    ind = floor(np.arange(onset-fs,onset+fs,1))
-    ind.astype(int)
-    t = ind/200.0;
+    ind = np.arange(onset-fs,onset+fs,1)
+    ind = ind.astype(int)
+    t = ind/float(fs) - onset/fs
     
+    
+    fig, axes = plt.subplots(figsize=(30,24))
+    axes.axis('off')
     for e in range(256):
-        fig, axes = plt.subplots(figsize=(36,24))
+
         ax = fig.add_subplot(16,16,e)
         d = ERP[e,ind]
         err = ERPerr[e,ind]
         plt.plot(t,d)
         plt.fill_between(t,d-err,d+err)
-        plt.ylim([minall,maxall])
-        fig.subplots_adjust(hspace=.05) # Put some space between the plots for ease of viewing
+        plt.ylim([-1,2])
+        if e == 0:
+            plt.yticks([-1,2])
+        elif (e+1)%16.0 == 0:
+            plt.xticks([-1,0,1])
+        else:
+            plt.xticks([])
+            plt.yticks([])
         
-    #rcParams['font.family'] = 'sans-serif'
-    #rcParams['text.usetex'] = False
-    #rcParams['axes.labelsize'] = 10
-    #rcParams['xtick.labelsize'] = 10
-    #rcParams['ytick.labelsize'] = 10
-    #rcParams['legend.fontsize'] = 10
+        
+    plt.tight_layout()    
+        
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['text.usetex'] = False
+    rcParams['axes.labelsize'] = 10
+    rcParams['xtick.labelsize'] = 10
+    rcParams['ytick.labelsize'] = 10
+    rcParams['legend.fontsize'] = 10
         
     #savefig("EC34_Vowel_ERPs.pdf") 
     
